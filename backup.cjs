4be8649328570b3e27348517bfc7c4af0c1d7e9d
Qwantums <qwantumsyt@gmail.com>
2025-02-21 11:45:56 -0600

    BSON compression for backup

--- backup.cjs
+++ backup.cjs
@@ -1,7 +1,35 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const BSON = require('bson');
 
 module.exports.autoSave = async function() {
+    //  Compresses all JSONs > 1 BSON
+    const dataFolderPath = path.join(__dirname, 'data');
+    const dataFolder = fs.reddir(dataFolderPath);
+    const saveObj = [];
+    for (const file in dataFolder) {
+        const filePath = path.join(dataFolderPath, file);
+        const fileName = path.parse(file).name;
+        fs.readFile(filePath, 'utf-8', (err, jsonContent) => {
+            if (err) {
+                throw new Error('Error reading file:', err);
+            }
+            let jsonData = JSON.parse(jsonContent);
+            jsonData = { file: fileName }; //   only works on arrays (works for now but might not l8r)
+            saveObj[0].push(jsonData);
+        });
+    }
+    const bsonData = BSON.serialize(saveObj);
+    const filePath = path.join(dataFolderPath, 'backup.bson');
+    fs.writeFile(filePath, bsonData, (err) => {
+        if (err) {
+            throw new Error('Error saving backup:', err);
+        } else {
+            console.log('Backed up JSONs!');
+        }
+    });
+
+    /*  Saves 1 JSON > 1 JSON
     const dataPath = path.join(__dirname, 'data');
     const backFolderPath = path.join(dataPath, 'backup');
     const dataFile = fs.readdirSync(dataPath).filter(file => file.endsWith('.json'));
@@ -13,7 +41,7 @@ module.exports.autoSave = async function() {
                     throw new Error(`Couldn't read ${filePath}...`, err);
                 }
             });
-            const backPath = path.join(backFolderPath, `${file}.backup`);
+            const backPath = path.join(backFolderPath, `${file}`);
             fs.writeFile(backPath, data, (err) => {
                 if (err) {
                     throw new Error(`Couldn't read ${filePath}...`, err);
@@ -23,4 +51,5 @@ module.exports.autoSave = async function() {
     } catch (err) {
         console.error('Error:', err);
     }
+    */
 };
